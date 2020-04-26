5fd7badd1e8e5dac452e28d2050b2aa821d27f59
Marcelo Santos <immarcelosantos@gmail.com>
2020-04-26 13:58:18 -0300

    Add summary with sum of all cases

--- lib/covid19_web/live/page_live.ex
+++ lib/covid19_web/live/page_live.ex
@@ -9,14 +9,19 @@ defmodule Covid19Web.PageLive do
   def mount(_params, _session, socket) do
     if connected?(socket), do: :timer.send_interval(@update_interval, self(), :update)
 
-    {:ok, assign(socket, results: fetch_results())}
+    data = fetch_results()
+    {:ok, assign(socket, [results: data, summary: fetch_summary(data)])}
   end
 
   @impl true
   def handle_info(:update, socket) do
     Logger.info("Preparing update...")
-    {:noreply, assign(socket, :results, fetch_results())}
+    data = fetch_results()
+
+    {:noreply, assign(socket, [results: data, summary: fetch_summary(data)])}
   end
 
   defp fetch_results, do: Covid19.list_all()
+
+  defp fetch_summary(results), do: Covid19.aggregate(results)
 end
